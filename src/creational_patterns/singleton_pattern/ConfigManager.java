0cfa71966896853ee666ca7de3f65ec190c83fcc
God-Incarnate <prashant11111999@gmail.com>
2026-03-12 21:13:02 +0530

    Creational Pattern: [Singleton] Config Manager Singleton

--- src/creational_patterns/singleton_pattern/ConfigManager.java
+++ src/creational_patterns/singleton_pattern/ConfigManager.java
@@ -6,5 +6,18 @@ import java.util.Properties;
 
 
 public class ConfigManager {
+    private ConfigManager instance;
+    private Properties properties=new Properties();
+
+    private ConfigManager(){
+        try{
+            FileInputStream configFile=new FileInputStream("config.properties");
+            properties.load(configFile);
+            Logger logger=Logger.getInstance();
+            logger.info("Configuration Loaded");
+        } catch (IOException e){
+            throw new RuntimeException("Configuration cannot be loaded");
+        }
+    }
 
 }
