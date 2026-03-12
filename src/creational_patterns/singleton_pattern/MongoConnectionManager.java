1eb5df3fd3e94f434d4b9757795720bdfc8cb665
God-Incarnate <prashant11111999@gmail.com>
2026-03-12 21:04:21 +0530

    Creational Pattern: [Singleton] MongoConnection Manager Singleton

---
+++ src/creational_patterns/singleton_pattern/MongoConnectionManager.java
@@ -0,0 +1,11 @@
+package creational_patterns.singleton_pattern;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+
+
+public class MongoConnectionManager {
+
+
+}
