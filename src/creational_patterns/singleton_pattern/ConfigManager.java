ff72dcf1ab470f75ff38b31a603e47a31cb2105b
God-Incarnate <prashant11111999@gmail.com>
2026-03-12 21:14:07 +0530

    Creational Pattern: [Singleton] Config Manager Singleton

--- src/creational_patterns/singleton_pattern/ConfigManager.java
+++ src/creational_patterns/singleton_pattern/ConfigManager.java
@@ -4,7 +4,37 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
+/*
+Usage:::::::::::::
+            public class DatabaseService {
 
+                public void connect() {
+
+                    ConfigManager config =
+                            ConfigManager.getInstance();
+
+                    String url = config.getProperty("db.url");
+                    String username = config.getProperty("db.username");
+
+                    System.out.println("Connecting to DB: " + url);
+                    System.out.println("Username: " + username);
+                }
+            }
+
+Configuration is loaded one time.
+
+Application
+   │
+   ├ UserService
+   ├ PaymentService
+   └ OrderService
+         │
+         ▼
+   ConfigManager (Singleton)
+         │
+     config.properties
+
+ */
 public class ConfigManager {
     private ConfigManager instance;
     private Properties properties=new Properties();
