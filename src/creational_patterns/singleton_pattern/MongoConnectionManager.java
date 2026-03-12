e8eccc886cd497c31ad8a2bac81009619d150fe8
God-Incarnate <prashant11111999@gmail.com>
2026-03-12 21:05:05 +0530

    Creational Pattern: [Singleton] MongoConnection Manager Singleton

--- src/creational_patterns/singleton_pattern/MongoConnectionManager.java
+++ src/creational_patterns/singleton_pattern/MongoConnectionManager.java
@@ -4,7 +4,35 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
 
+/*
+-----------------------------------MONGODB---------------------------------------
+Usage:::::::::::::::
+            import com.mongodb.client.MongoCollection;
+            import com.mongodb.client.MongoDatabase;
+            import org.bson.Document;
 
+            public class UserService {
+
+                public void insertUser() {
+
+                    MongoDatabase database =
+                            MongoConnectionManager
+                                    .getInstance()
+                                    .getDatabase();
+
+                    MongoCollection<Document> collection =
+                            database.getCollection("users");
+
+                    Document user = new Document()
+                            .append("name", "Prashant")
+                            .append("age", 25);
+
+                    collection.insertOne(user);
+
+                    System.out.println("User inserted");
+                }
+            }
+ */
 public class MongoConnectionManager {
 
 
