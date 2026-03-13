c3a98d77c492591e9606de081b571986bb409161
God-Incarnate <prashant11111999@gmail.com>
2026-03-13 20:25:29 +0530

    Creational Pattern: [Singleton] Bill Pugh Singleton

--- src/creational_patterns/singleton_pattern/BillPughSingleton.java
+++ src/creational_patterns/singleton_pattern/BillPughSingleton.java
@@ -2,4 +2,20 @@ package creational_patterns.singleton_pattern;
 
 
 public class BillPughSingleton {
+    private BillPughSingleton() {}
+
+    private static class SingletonHelper {
+
+        private static final BillPughSingleton INSTANCE =
+                new BillPughSingleton();
+    }
+
+    public static BillPughSingleton getInstance() {
+
+        return SingletonHelper.INSTANCE;
+    }
+
+    public void printMessage() {
+        System.out.println("Bill Pugh Singleton Instance");
+    }
 }
