f4e23313eeafa3e683e95691442b91b83daa2513
God-Incarnate <prashant11111999@gmail.com>
2026-03-20 15:18:04 +0530

    Structural Pattern: Adapter

--- src/structural_patterns/Adapter/SMSAdapter.java
+++ src/structural_patterns/Adapter/SMSAdapter.java
@@ -3,7 +3,37 @@ package structural_patterns.Adapter;
 import DTO.Notification;
 import DTO.SMSNotification;
 
+/*
+1️⃣ Adapter Pattern
+Purpose
 
+The Adapter Pattern allows classes with incompatible interfaces to work together.
+
+It acts as a wrapper that converts one interface into another expected by the client.
+
+Real-World Analogy:::::::::::::
+
+A phone charger adapter converts:
+
+220V Socket → 5V USB
+
+Structure::::::::::::
+Client → Target Interface → Adapter → Adaptee
+
+Usage:::::::::::::::::::
+public class AdapterDemo {
+
+    public static void main(String[] args) {
+
+        LegacySMSService legacy = new LegacySMSService();
+
+        NotificationService service =
+                new SMSAdapter(legacy);
+
+        service.send("Hello");
+    }
+}
+ */
 class SMSAdapter implements Notification {
 
     private SMSNotification smsService;
